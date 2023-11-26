cf7fca90dcdbfa7cd7ff8b363d32e8ac7dd7aeec
Musab Habeeb <habeebmusab@gmail.com>
2023-11-26 13:03:01 +0100

    done with chat UI

--- happymed/src/components/chat/subcomponent/ChatHeader.js
+++ happymed/src/components/chat/subcomponent/ChatHeader.js
@@ -1,10 +1,10 @@
-import Settings from "../../../images/chat/settings.svg";
-import Download from "../../../images/chat/download.svg";
-import Profile from "../../../images/chat/profile.svg";
+import Settings from "../../../images/chat/chatheader/settings.svg";
+import Download from "../../../images/chat/chatheader/download.svg";
+import Profile from "../../../images/chat/chatheader/profile.svg";
 
 const ChatHeader = () => {
     return (
-        <div className="bg-[#FFFFFF] w-[100%] py-4 px-8 flex items-center justify-end gap-2">
+        <div className="bg-[#FFFFFF] z-50 w-[100%] py-4 px-8 flex items-center justify-end gap-2">
             <img src={Settings} alt="A settings icon" />
             <img src={Download} alt="A download icon" />
             <img src={Profile} alt="A profile icon" />
